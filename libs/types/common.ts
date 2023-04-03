62f3da40c34f4d917e64b92789b28f89ed0030f2
Chris St Jean <chris.stjean@33floors.com>
2023-04-02 20:08:10 -0700

    testing libs trigger

--- libs/types/common.ts
+++ libs/types/common.ts
@@ -1,21 +1,22 @@
-import { Request } from 'express';
+import { Request } from "express";
 
 export interface EnvironmentVariables {
-  MYSQL_USER_PASSWORD: string;
-  MYSQL_HOST: string;
-  MYSQL_USER_NAME: string;
-  MYSQL_DB: string;
-  JWT_SECRET: string;
-  GOOGLE_CLIENT_ID: string;
-  GOOGLE_CLIENT_SECRET: string;
-  GOOGLE_REDIRECT: string;
-  PROJECT_ID: string;
+    MYSQL_USER_PASSWORD: string;
+    MYSQL_HOST: string;
+    MYSQL_USER_NAME: string;
+    MYSQL_DB: string;
+    JWT_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    GOOGLE_REDIRECT: string;
+    PROJECT_ID: string;
+    ENV_TYPE?: string;
 }
 
 export interface JwtPayload {
-  email: string;
+    email: string;
 }
 
 export interface AuthorizedRequest extends Request {
-  payload?: JwtPayload;
+    payload?: JwtPayload;
 }
