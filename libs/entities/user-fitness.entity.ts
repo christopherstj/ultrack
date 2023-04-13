27e85923e50a16f10c96e140a36956f1e8afae65
Chris St Jean <chris.stjean@33floors.com>
2023-04-13 13:56:07 -0700

    adding entities to lib

---
+++ libs/entities/user-fitness.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'details_userFitness',
+})
+export class UserFitness {
+  @PrimaryGeneratedColumn()
+  userId: string;
+
+  @Column({ type: 'decimal', precision: 8, scale: 5 })
+  thresholdPace?: number;
+
+  @Column()
+  thresholdPaceSource?: string;
+
+  @Column()
+  units: number;
+}
