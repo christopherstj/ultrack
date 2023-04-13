b5a6ebc7ceb1df8a1329538ea1446a364ace9d62
Chris St Jean <chris.stjean@33floors.com>
2023-04-13 16:28:11 -0700

    fixing workout

--- libs/entities/workout.entity.ts
+++ libs/entities/workout.entity.ts
@@ -1,10 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({
     name: "workouts_workout",
 })
 export class Workout {
-    @Column()
+    @PrimaryColumn()
     workoutId: string;
 
     @Column()
