8247db52d855409aa0b5726fe5c0c1389fc0f5e3
Chris St Jean <chris.stjean@33floors.com>
2023-04-13 15:35:01 -0700

    updating workout entity

--- libs/entities/workout.entity.ts
+++ libs/entities/workout.entity.ts
@@ -1,75 +1,75 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({
-  name: 'workouts_workout',
+    name: "workouts_workout",
 })
 export class Workout {
-  @PrimaryGeneratedColumn()
-  workoutId: string;
+    @PrimaryGeneratedColumn()
+    workoutId: string;
 
-  @Column()
-  userId: string;
+    @Column()
+    userId: string;
 
-  @Column()
-  distance: number;
+    @Column({ type: "decimal", precision: 10, scale: 2 })
+    distance: number;
 
-  @Column()
-  totalTime: number;
+    @Column({ type: "decimal", precision: 18, scale: 2 })
+    totalTime: number;
 
-  @Column()
-  trackedTime: number;
+    @Column({ type: "decimal", precision: 18, scale: 2 })
+    trackedTime: number;
 
-  @Column()
-  movingTime: number;
+    @Column({ type: "decimal", precision: 18, scale: 2 })
+    movingTime: number;
 
-  @Column()
-  avgCadence: number;
+    @Column()
+    avgCadence: number;
 
-  @Column()
-  avgHeartRate: number;
+    @Column()
+    avgHeartRate: number;
 
-  @Column()
-  avgSpeed: number;
+    @Column({ type: "decimal", precision: 8, scale: 5 })
+    avgSpeed: number;
 
-  @Column()
-  avgStepLength: number;
+    @Column({ type: "decimal", precision: 8, scale: 5 })
+    avgStepLength: number;
 
-  @Column()
-  maxCadence: number;
+    @Column()
+    maxCadence: number;
 
-  @Column()
-  maxSpeed: number;
+    @Column({ type: "decimal", precision: 8, scale: 5 })
+    maxSpeed: number;
 
-  @Column()
-  maxHeartRate: number;
+    @Column()
+    maxHeartRate: number;
 
-  @Column()
-  minHeartRate: number;
+    @Column()
+    minHeartRate: number;
 
-  @Column()
-  startTime: Date;
+    @Column()
+    startTime: Date;
 
-  @Column()
-  runType: string;
+    @Column()
+    runType: string;
 
-  @Column()
-  totalAscent: number;
+    @Column()
+    totalAscent: number;
 
-  @Column()
-  totalDescent: number;
+    @Column()
+    totalDescent: number;
 
-  @Column()
-  totalCalories: number;
+    @Column()
+    totalCalories: number;
 
-  @Column()
-  thresholdPace: number;
+    @Column({ type: "decimal", precision: 8, scale: 5 })
+    thresholdPace: number;
 
-  @Column()
-  effectivePace: number;
+    @Column({ type: "decimal", precision: 8, scale: 5 })
+    effectivePace: number;
 
-  @Column()
-  trainingStress: number;
+    @Column()
+    trainingStress: number;
 
-  @Column()
-  effectiveIntensity: number;
+    @Column({ type: "decimal", precision: 4, scale: 2 })
+    effectiveIntensity: number;
 }
