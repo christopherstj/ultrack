56a080b1d379a87afd72ecb864670c91b93790ef
Chris St. Jean <chris.r.stjean@gmail.com>
2023-04-23 12:20:58 -0700

    adding date query parsing

--- libs/utils/utils.ts
+++ libs/utils/utils.ts
@@ -1,2 +1,5 @@
 export const mpsToMinsPerMile = (mps: number) => 26.8224 / mps;
 export const minsPerMileToMps = (mins: number) => 26.8224 / mins;
+
+export const formatDateForQuery = (date: Date) =>
+  `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
