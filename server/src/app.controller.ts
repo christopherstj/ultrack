603b57a052cdd59a4f4c69644940351b69d59f6a
Chris St. Jean <chris.r.stjean@gmail.com>
2023-06-06 14:26:15 -0700

    hehe

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get('/')
   async test(@Req() req: Request, @Res() res: Response) {
-    res.status(200).send('Look Mom I Made It');
+    res.status(200).send('This was harder to do than Cocodona');
   }
 }
