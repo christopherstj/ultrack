18bded5ce09edb54eab3a0294325d302433ea4b4
Chris St. Jean <chris.r.stjean@gmail.com>
2023-06-08 14:09:25 -0700

    updating ports

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,34 +1,39 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 import { WinstonModule } from 'nest-winston';
 import { AppModule } from './app.module';
 import { createLogger, EnvironmentVariables } from '@ultrack/libs';
 import { ConfigService } from '@nestjs/config';
+import * as https from 'https';
+import * as http from 'http';
+import * as express from 'express';
+import certManager from '@google-cloud/certificate-manager';
 
 const bootstrap = async () => {
   const appConfig = await NestFactory.create(AppModule);
   const configService = appConfig.get(ConfigService<EnvironmentVariables>);
 
-  const app1 = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: WinstonModule.createLogger({
-      instance: createLogger(
-        configService.get('PROJECT_ID')!,
-        'dev',
-        'http-server',
-      ),
-    }),
-  });
+  const server = express();
 
-  const app2 = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: WinstonModule.createLogger({
-      instance: createLogger(
-        configService.get('PROJECT_ID')!,
-        'dev',
-        'https-server',
-      ),
-    }),
-  });
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+    {
+      logger: WinstonModule.createLogger({
+        instance: createLogger(
+          configService.get('PROJECT_ID')!,
+          'dev',
+          'http-server',
+        ),
+      }),
+    },
+  );
 
-  await Promise.all([app2.listen(443), app1.listen(80)]);
+  await app.init();
+
+  http.createServer(server).listen(80);
 };
 bootstrap();
