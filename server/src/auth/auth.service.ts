ec827683f188e8bd75520a7676f57acc71d61bc7
Chris St. Jean <chris.r.stjean@gmail.com>
2023-06-12 20:43:34 -0700

    improving error logging

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -44,24 +44,20 @@ export class AuthService {
   // }
 
   async signIn(email: string, pass: string): Promise<any> {
-    try {
-      const user = await firstValueFrom(
-        this.usersServiceClient.send('local/find-one', { email }),
-      );
+    const user = await firstValueFrom(
+      this.usersServiceClient.send('local/find-one', { email }),
+    );
 
-      if (!user) throw new UnauthorizedException();
+    if (!user) throw new UnauthorizedException();
 
-      const success = await bcrypt.compare(pass, user.hashedPassword);
+    const success = await bcrypt.compare(pass, user.hashedPassword);
 
-      if (!success) throw new UnauthorizedException();
+    if (!success) throw new UnauthorizedException();
 
-      const payload = { email: user.email };
+    const payload = { email: user.email };
 
-      return {
-        accessToken: await this.jwtService.signAsync(payload),
-      };
-    } catch (err) {
-      console.error(err);
-    }
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+    };
   }
 }
