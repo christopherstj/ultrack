971b24a25c11b5cdd6bb1983279002470f218b4c
Chris St. Jean <chris.r.stjean@gmail.com>
2023-07-08 12:38:03 -0700

    updating error type

--- libs/types/success.ts
+++ libs/types/success.ts
@@ -1,4 +1,7 @@
+import { ErrorDetail } from "./error";
+
 export interface SuccessMessage {
-    success: boolean;
-    msg?: string;
+  success: boolean;
+  msg?: string;
+  errorList?: ErrorDetail[];
 }
